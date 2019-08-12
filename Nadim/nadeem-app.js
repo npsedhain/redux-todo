ede53e9c371b216e4a50e6354b9e9abc6ae6ef55
Nadeem Shakya <shakya.nadim12@gmail.com>
2019-08-12 17:25:37 +0545

    Added AddItem section

--- Nadim/nadeem-app.js
+++ Nadim/nadeem-app.js
@@ -1,32 +1,72 @@
-import {html, PolymerElement} from '@polymer/polymer/polymer-element.js';
+import { html, LitElement } from "lit-element";
+import "@polymer/paper-input/paper-input.js";
+import "@polymer/paper-button/paper-button.js";
+import "@polymer/paper-checkbox/paper-checkbox.js";
+import "@polymer/paper-radio-button/paper-radio-button.js";
 
-/**
- * `nadeem-app`
- * 
- *
- * @customElement
- * @polymer
- * @demo demo/index.html
- */
-class NadeemApp extends PolymerElement {
-  static get template() {
-    return html`
-      <style>
-        :host {
-          display: block;
-        }
-      </style>
-      <h2>Hello [[prop1]]!</h2>
-    `;
-  }
+const VisibilityFilters = {
+  SHOW_ALL: "All",
+  SHOW_ACTIVE: "Active",
+  SHOW_COMPLETED: "Completed",
+};
+
+class NadeemApp extends LitElement {
   static get properties() {
     return {
-      prop1: {
-        type: String,
-        value: 'nadeem-app',
-      },
+      todos: Array,
+      filter: String,
+      task: String,
     };
+  }
+
+  constructor() {
+    super();
+    this.todos = [];
+    this.filter = VisibilityFilters.SHOW_ALL;
+    this.task = "";
+  }
+
+  addTodo(e) {
+    if (this.task) {
+      this.todos = [
+        ...this.todos,
+        {
+          task: this.task,
+          completed: false,
+        },
+      ];
+      this.task = "";
+      console.log(this.todos);
+    }
+  }
+
+  handleChange(e) {
+    if (e.keyCode === 13) {
+      return this.addTodo();
+    }
+    this.task = e.target.value;
+  }
+  render() {
+    return html`
+      <div class="input-layout">
+        <paper-input
+          always-float-label
+          label="Floating label"
+          .value="${this.task}"
+          @click="${this.addTodo}"
+          @keyup="${this.handleChange}"
+        ></paper-input>
+
+        <paper-button
+          raised
+          class="indigo"
+          value="Nadim"
+          @click="${this.addTodo}"
+          >raised</paper-button
+        >
+      </div>
+    `;
   }
 }
 
-window.customElements.define('nadeem-app', NadeemApp);
+window.customElements.define("nadeem-app", NadeemApp);
